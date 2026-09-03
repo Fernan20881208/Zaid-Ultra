0f8420f68e083909af08aac865db16de5261b2b5
Zaid.nvr <utegfer@gmail.com>
2026-09-03 13:07:13 -0600

    feat: add per-level performance heatmap telemetry

--- src/core/Telemetry.hpp
+++ src/core/Telemetry.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <cstddef>
 #include <deque>
 
@@ -13,15 +14,28 @@ struct FrameSnapshot {
     int spikeCount = 0;
 };
 
+struct SectionSnapshot {
+    bool valid = false;
+    float startPercent = 0.f;
+    float endPercent = 0.f;
+    float averageFrameMs = 0.f;
+    float worstFrameMs = 0.f;
+    float loadRatio = 0.f;
+    std::size_t samples = 0;
+};
+
 class Telemetry final {
 public:
     static Telemetry& get();
 
+    void resetLevel();
     void resetAttempt();
     void sample(float dt, float percent);
 
     [[nodiscard]] FrameSnapshot snapshot(std::size_t window = 240) const;
     [[nodiscard]] float performanceScore(float targetFps) const;
+    [[nodiscard]] float levelPerformanceScore(float targetFps) const;
+    [[nodiscard]] SectionSnapshot worstSection(float targetFps, int sectionWidth = 5) const;
     [[nodiscard]] float currentPercent() const;
 
 private:
@@ -30,7 +44,14 @@ private:
         float percent;
     };
 
+    struct LevelBucket {
+        double totalFrameMs = 0.0;
+        float worstFrameMs = 0.f;
+        std::size_t samples = 0;
+    };
+
     std::deque<Sample> m_samples;
+    std::array<LevelBucket, 101> m_levelBuckets{};
     float m_currentPercent = 0.f;
     static constexpr std::size_t kMaxSamples = 3600;
 };
